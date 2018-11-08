0d155fa038b62f9e1b2dbe839a0913349fa9c234
David <david@thisisparachute.com>
2018-11-08 11:49:54 +0000

    connection settings change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const expo = new Expo();
 const PORT_NUMBER = 3000;
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://localhost/goals_pn_server';
+var mongoDB = 'mongodb://parachute:Uzx**978@ds257333-a0.mlab.com:57333,ds257333-a1.mlab.com:57333/heroku_cngjj092?replicaSet=rs-ds257333';
 
 mongoose.connect(mongoDB);
 // Get Mongoose to use the global promise library
@@ -19,14 +19,7 @@ mongoose.Promise = global.Promise;
 //Get the default connection
 var db = mongoose.connection;
 
-
-
 let users = User.find({});
-
-
-
-
-
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
