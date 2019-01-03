d0cfdd598c9db010ed1f54dad2eeafc04823c7f3
David <david@thisisparachute.com>
2019-01-03 18:45:51 +0000

    latest changes

--- index.js
+++ index.js
@@ -301,16 +301,16 @@ const saveToken = (token, device_type, member_id, res) => {
         let user = {
          first_name: doc[0].first_name,
          last_name: doc[0].last_name,
-       };
-
-        let club = {
          name: doc[0].first_name + " " + doc[0].last_name,
          email: club_email,
          club: club_id
+
        };
 
+      
+
        sendEmail("david@thisisparachute.com", "You've won a free game at Goals!", user, "winner");
-      sendEmail("david@thisisparachute.com", "Someone's won a free game", club, "winner-club");
+      sendEmail("david@thisisparachute.com", "Someone's won a free game", user, "winner-club");
 
     handlePushTokens("You've won a FREE game! Just use the QuickPay option in the app to redeem.", "You've won a free game at Goals!", member_id);
 
