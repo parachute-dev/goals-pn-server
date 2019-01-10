fb7f477b8c2ca55fb1babf1035a8ec5afac40d6b
David <david@thisisparachute.com>
2019-01-10 22:48:52 +0000

    latet changes

--- index.js
+++ index.js
@@ -670,7 +670,7 @@ app.post('/winners/choose', (req, res) => {
   var d = new Date();
   var n = d.getHours();
 
-  if (req.get('api-key') == apikey ) {
+  if (req.get('api-key') == apikey && n > 24) {
     getTonightsWinners();
     generateReport();
     res.send(`Getting Tonights Winners, ${req.body.member_id}`);
