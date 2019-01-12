6ae3a79a5099aed5a8ffdf5f7748b9e3abc75438
David <david@thisisparachute.com>
2019-01-12 22:15:21 +0000

    changes

--- index.js
+++ index.js
@@ -669,7 +669,7 @@ app.post('/winners/choose', (req, res) => {
   var d = new Date();
   var n = d.getHours();
 
-  if (req.get('api-key') == apikey && n > 25) {
+  if (req.get('api-key') == apikey ) {
     getTonightsWinners();
     generateReport();
     res.send(`Getting Tonights Winners, ${req.body.member_id}`);
